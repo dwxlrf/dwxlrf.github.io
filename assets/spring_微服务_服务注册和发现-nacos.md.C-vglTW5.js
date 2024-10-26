277b29c1b61301356147dfe4e0a6bf6c7a4a79e5
dwxlrf <dwxlrf@users.noreply.github.com>
2024-10-26 16:42:45 +0000

    ms

---
+++ assets/spring_微服务_服务注册和发现-nacos.md.C-vglTW5.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.DPuwY6B9.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"spring/微服务/服务注册和发现-nacos.md","filePath":"spring/微服务/服务注册和发现-nacos.md"}'),s={name:"spring/微服务/服务注册和发现-nacos.md"};function n(o,r,c,_,p,i){return a(),t("div")}const f=e(s,[["render",n]]);export{m as __pageData,f as default};
